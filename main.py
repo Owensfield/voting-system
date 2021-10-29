baa5a11f876301674caba8effe7f429a5f00bc2f
benarc <ben@arc.wales>
2021-10-29 19:43:26 +0100

    Basic view

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = {"page": "Home page"}
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
